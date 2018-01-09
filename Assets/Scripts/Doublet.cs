3f28aa3ab573b13b3496aaa97269813c39a92c13
tristan-briant <tristan.briant@upmc.fr>
2018-01-09 18:36:15 +0100

    doublet

---
+++ Assets/Scripts/Doublet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doublet : MonoBehaviour {
+
+    public GameObject  Atome;
+    public GameObject doublet;
+
+    public float distance = 50; // distance from element
+
+    void CreateDoublet()
+    {
+        GameObject go = Instantiate(Resources.Load("Doublet")) as GameObject; ;
+    }
+
+
+}
